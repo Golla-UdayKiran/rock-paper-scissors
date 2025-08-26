7b8067a2bf8a393981c887e4533b58cd8d9e6414
Uday Kiran <uday@ceooftech.com>
2025-08-26 19:27:44 +0530

    images folder and css added

--- script.js
+++ script.js
@@ -63,7 +63,10 @@ function playGame(playerMove) {
 
   document.querySelector('.js-result').innerHTML = result;
 
-  document.querySelector('.js-moves').innerHTML = `You ${playerMove} - ${computerMove} Computer`;
+  document.querySelector('.js-moves').innerHTML = `You
+<img src="images/${playerMove}-emoji.png" class="move-icon">
+<img src="images/${computerMove}-emoji.png" class="move-icon">
+Computer`;
 }
 
 function updateScoreElement() {
